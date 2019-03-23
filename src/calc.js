924ed31856946875973690d1e550f5f1c46776b3
Nathaniel Hudson <ibn0721@gmail.com>
2019-03-23 12:39:56 -0500

    added tip calc js but jquary cdn still not working correctly

---
+++ src/calc.js
@@ -0,0 +1,19 @@
+function calculate(){
+    // var amount is used to retrive values from the amount and % fields and store them inside variables
+    // $ dollare sign is a reff to the j query library
+    var amount = $('#amount').val;
+    // % var does the same thing as amount and .val will calculate the value
+    var percentage = $('#percentage').val();
+    // amount is a string so it needs to be converted in the number function below.
+    var tip = amount * (percentage / 100);
+    var total = Number(amount) + tip;
+    // toFixed action function is a a number property to ensure two digits after decimal
+
+    $('#tip').val(tip.toFixed(2));
+    $('#total').val(total.toFixed(2));
+    // return used to prevent submission, browser wont return a new page because of any code below.
+    return false;
+}
+
+$('#calculator').submit(calculate);
+
